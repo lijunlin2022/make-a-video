41f3df78b85cf0404be98739f19ad785acdaf139
lijunlin <lijunlin2022@gmail.com>
2025-01-12 15:58:03 +0800

    feat: add react-router for page navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/home'
+import SVG from './pages/svg'
+import Video from './pages/video'
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline bg-red-100">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/svg" element={<SVG />} />
+        <Route path="/video" element={<Video />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
